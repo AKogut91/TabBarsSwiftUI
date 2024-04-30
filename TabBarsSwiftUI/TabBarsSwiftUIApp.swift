a38c57d0f43c969b9461e111ea783cabe30b46a4
Alex_Kogut13 <akogut@engenious.io>
2024-04-30 10:46:44 +0300

    Initial Commit

---
+++ TabBarsSwiftUI/TabBarsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarsSwiftUIApp.swift
+//  TabBarsSwiftUI
+//
+//  Created by Alex Kogut on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TabBarsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
